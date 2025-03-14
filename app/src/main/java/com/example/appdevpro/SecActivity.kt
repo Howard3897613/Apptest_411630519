87f8bb3518ceec17c7e188f20e758bc8b27fa9cc
陳庭浩 <112939670+Howard3897613@users.noreply.github.com>
2025-03-14 11:48:18 +0800

    0314

--- app/src/main/java/com/example/appdevpro/SecActivity.kt
+++ app/src/main/java/com/example/appdevpro/SecActivity.kt
@@ -1,29 +1,21 @@
 package com.example.appdevpro
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SecActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sec)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btnChange =findViewById<Button>(R.id.btnChange)
-        val b = Bundle()
-        btnChange.setOnClickListener{
-            val intent = Intent(this,SecActivity::class.java)
-            intent
-            startActivity(intent)
-        }
+
     }
 }
